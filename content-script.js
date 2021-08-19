48eeb7bb1bc1e7693e3aed46a40c904690d76de2
goDlin <godlin086@gmail.com>
2021-08-19 18:26:45 +0300

    get comments

--- content-script.js
+++ content-script.js
@@ -1,7 +1,23 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.method === 'getHTML') {
-        
+        const items = [...document.querySelector('.comments__content').querySelectorAll('.comments__item')]
+        const data = items.map(item => {
+            if (item.querySelector('.comments__item__user__image'))
+                return {
+                    id: item.getAttribute('data-id'),
+                    date: item.getAttribute('data-date'),
+                    userid: item.getAttribute('data-user_id'),
+                    rating: item.getAttribute('data-rating'),
+                    replyto: item.getAttribute('data-reply_to'),
+                    text: item.querySelector('.comments__item__text').innerHTML,
+                    img: item.querySelector('.comments__item__user__image').getAttribute('src'),
+                    username: item.querySelector('.user_name').innerText,
+                    userlink: item.querySelector('.comments__item__user').getAttribute('href'),
+                    time: item.querySelector('time').innerText,
+                    votevalue: item.querySelector('.vote__value__v').innerText
+                }
+        })
 
-        sendResponse({ data: document.querySelector('.comments__content').outerHTML })
+        sendResponse({ comments: data.sort((a, b) => b.rating - a.rating) })
     }
 })
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
 chrome.tabs.sendMessage(tab.id, { method: 'getHTML' }, (response) => {
+    const container = document.querySelector('#comments')
 
-    console.log(response)
-    console.log('new')
-    
+    for (let index = 0; index < 5; index++) {
+        const item = response.comments[index]
+        
+        let html = '<div class="comment">'
+        html += '<div><img class="user__image" src="' + item.img + '"></div>'
+        html += '<div><a class="user" href="' + item.userlink + '"><div>' + item.username + '</div>'
+        html += '<time>' + item.time + '</time></a></div>'
+        html += '<div class="vote__value">' + item.votevalue + '</div>'
+        html += '<div class="text">' + item.text + '</div></div>'
+
+        container.innerHTML += html
+    }
+
 })
