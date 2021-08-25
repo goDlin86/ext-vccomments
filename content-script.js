aac0aac9a151fa46149c0a9ae4c17f87ca3aecd3
goDlin <godlin086@gmail.com>
2021-08-25 18:14:31 +0300

    fix avatar image

--- content-script.js
+++ content-script.js
@@ -1,23 +1,27 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.method === 'getHTML') {
         const items = [...document.querySelector('.comments__content').querySelectorAll('.comments__item')]
-        const data = items.map(item => {
-            if (item.querySelector('.comments__item__user__image'))
-                return {
+        const data = items.reduce((result, item) => {
+            if (item.querySelector('.comments__item__user__image, .comments__item__avatar > .andropov_image'))
+                result.push({
                     id: item.getAttribute('data-id'),
                     date: item.getAttribute('data-date'),
                     userid: item.getAttribute('data-user_id'),
                     rating: item.getAttribute('data-rating'),
                     replyto: item.getAttribute('data-reply_to'),
                     text: item.querySelector('.comments__item__text').innerHTML,
-                    img: item.querySelector('.comments__item__user__image').getAttribute('data-image-src'),
+                    img: item.querySelector('.comments__item__user__image, .comments__item__avatar > .andropov_image').getAttribute('data-image-src'),
                     username: item.querySelector('.user_name').innerText,
                     userlink: item.querySelector('.comments__item__user').getAttribute('href'),
                     time: item.querySelector('time').innerText,
                     votevalue: item.querySelector('.vote__value__v').innerText,
-                    media: item.querySelector('.comments__item__media > .andropov_image') ? item.querySelector('.comments__item__media > .andropov_image').getAttribute('data-image-src') : null
-                }
-        })
+                    media: 
+                        item.querySelector('.comments__item__space').querySelector('.comments__item__media > .andropov_image') ? 
+                        item.querySelector('.comments__item__space').querySelector('.comments__item__media > .andropov_image').getAttribute('data-image-src') : null
+                })
+            
+            return result
+        }, [])
 
         sendResponse({ comments: data.sort((a, b) => b.votevalue - a.votevalue) })
     }
--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
 chrome.tabs.sendMessage(tab.id, { method: 'getHTML' }, (response) => {
     const container = document.querySelector('#comments')
+    console.log(response.comments)
 
-    for (let index = 0; index < 5; index++) {
+    const k = response.comments.length > 5 ? 5 : response.comments.length
+    for (let index = 0; index < k; index++) {
         const item = response.comments[index]
         
         let html = '<div class="comment">'
         html += '<div><img class="user__image" src="' + item.img + '-/scale_crop/64x64/-/format/webp/"></div>'
-        html += '<div><a class="user" href="' + item.userlink + '" target="_blank"><div>' + item.username + '</div>'
-        html += '<time>' + item.time + '</time></a></div>'
+        html += '<div><a class="user" href="' + item.userlink + '" target="_blank">' + item.username + '</a>'
+        html += '<time>' + item.time + '</time></div>'
         html += '<div class="vote__value">' + item.votevalue + '</div>'
         html += '<div class="text">' + item.text + '</div>'
         if (item.media) html += '<img src="' + item.media + '-/preview/480/-/format/webp/">'
