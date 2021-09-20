ca01bb3f4073bc3061380a6ba099e9aa83224b56
goDlin <godlin086@gmail.com>
2021-09-20 17:22:05 +0300

    new comment class

--- content-script.js
+++ content-script.js
@@ -1,24 +1,24 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.method === 'getComments') {
         const title = document.querySelector('.content-title').innerText
-        const items = [...document.querySelector('.comments__content').querySelectorAll('.comments__item')]
+        const items = [...document.querySelector('.comments__content').querySelectorAll('.comment')]
         const data = items.reduce((result, item) => {
-            if (item.querySelector('.comments__item__user__image, .comments__item__avatar > .andropov_image'))
+            if (item.querySelector('.comment__avatar > .andropov_image'))
                 result.push({
                     id: item.getAttribute('data-id'),
                     date: item.getAttribute('data-date'),
                     userid: item.getAttribute('data-user_id'),
                     rating: item.getAttribute('data-rating'),
                     replyto: item.getAttribute('data-reply_to'),
-                    text: item.querySelector('.comments__item__text').innerHTML,
-                    img: item.querySelector('.comments__item__user__image, .comments__item__avatar > .andropov_image').getAttribute('data-image-src'),
-                    username: item.querySelector('.user_name').innerText,
-                    userlink: item.querySelector('.comments__item__user').getAttribute('href'),
+                    text: item.querySelector('.comment__text').innerHTML,
+                    img: item.querySelector('.comment__avatar > .andropov_image').getAttribute('data-image-src'),
+                    username: item.querySelector('.comment-user__name').innerText,
+                    userlink: item.querySelector('.comment-user').getAttribute('href'),
                     time: item.querySelector('time').innerText,
                     votevalue: item.querySelector('.vote__value__v').innerText,
                     media: 
-                        item.querySelector('.comments__item__space').querySelector('.comments__item__media > .andropov_image') ? 
-                        item.querySelector('.comments__item__space').querySelector('.comments__item__media > .andropov_image').getAttribute('data-image-src') : null
+                        item.querySelector('.comment__space').querySelector('.comment__media > .andropov_image') ? 
+                        item.querySelector('.comment__space').querySelector('.comment__media > .andropov_image').getAttribute('data-image-src') : null
                 })
             
             return result
