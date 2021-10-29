7b9a29d9dca270ac725373cfff282aaea3936112
goDlin <godlin086@gmail.com>
2021-10-29 16:55:27 +0300

    add video, add reply

--- content-script.js
+++ content-script.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.method === 'getComments') {
-        const title = document.querySelector('.content-title').innerText
+        const title = document.querySelector('.content-title') ? document.querySelector('.content-title').innerText : ''
         const items = [...document.querySelector('.comments__content').querySelectorAll('.comment')]
         const data = items.reduce((result, item) => {
-            if (item.querySelector('.comment__avatar > .andropov_image'))
+            if (item.querySelector('.comment__avatar .andropov_image'))
                 result.push({
                     id: item.getAttribute('data-id'),
                     date: item.getAttribute('data-date'),
@@ -16,15 +16,40 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                     userlink: item.querySelector('.comment-user').getAttribute('href'),
                     time: item.querySelector('time').innerText,
                     votevalue: item.querySelector('.vote__value__v').innerText,
-                    media: 
-                        item.querySelector('.comment__space').querySelector('.comment__media > .andropov_image') ? 
-                        item.querySelector('.comment__space').querySelector('.comment__media > .andropov_image').getAttribute('data-image-src') : null
+                    image: 
+                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_image') ? 
+                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_image').getAttribute('data-image-src') : null,
+                    video: 
+                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_video') ? 
+                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_video').getAttribute('data-video-mp4') : null
                 })
             
             return result
         }, [])
 
-        const comments = data.sort((a, b) => parseFloat(b.votevalue) - parseFloat(a.votevalue))
+        const sorted = data.sort((a, b) => parseFloat(b.votevalue) - parseFloat(a.votevalue))
+        
+        const comments = sorted.slice(0, 7).reduce((result, item) => {
+            if (!result.includes(c => c.id === item.id)) {
+                let level = 0
+
+                let replyto = item.replyto
+                let parents = []
+                while (replyto !== '0') {
+                    const parent = sorted.find(c => c.id === replyto)
+                    replyto = parent.replyto
+                    parents.push(parent)
+                    level += 1
+                }
+                
+                result.push(...parents.reverse())
+
+                item.level = level
+                result.push(item)
+            }
+
+            return result
+        }, [])
 
         sendResponse({ title, comments })
     }
--- index.js
+++ index.js
@@ -6,20 +6,18 @@ chrome.tabs.sendMessage(tab.id, { method: 'getComments' }, (response) => {
 
     console.log(response.comments)
 
-    const k = response.comments.length > 7 ? 7 : response.comments.length
-    for (let index = 0; index < k; index++) {
-        const item = response.comments[index]
-        
-        let html = '<div class="comment">'
+    response.comments.map(item => {        
+        let html = '<div class="comment" style="margin-left:' + (item.level*40) + 'px">'
         html += '<div><img class="user__image" src="' + item.img + '-/scale_crop/64x64/-/format/webp/"></div>'
         html += '<div><a class="user" href="' + item.userlink + '" target="_blank">' + item.username + '</a>'
         html += '<time>' + item.time + '</time></div>'
         html += '<div class="vote__value ' + (item.votevalue > 0 ? 'plus' : 'minus') + '"><span>' + item.votevalue + '</span></div>'
         html += '<div class="text">' + item.text + '</div>'
-        if (item.media) html += '<img src="' + item.media + '-/preview/480/-/format/webp/">'
+        if (item.image) html += '<img src="' + item.image + '-/preview/480/-/format/webp/">'
+        if (item.video) html += '<video autoplay="" loop="" playsinline="" muted=""><source src="' + item.video + '" type="video/mp4"></video>'
         html += '</div>'
 
         container.innerHTML += html
-    }
+    })
 
 })
