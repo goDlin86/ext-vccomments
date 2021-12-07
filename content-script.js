e41c0978af8119a7f5aeda29b186c6f12aa4a742
goDlin <godlin086@gmail.com>
2021-12-07 18:06:15 +0300

    new elements

--- content-script.js
+++ content-script.js
@@ -1,27 +1,27 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.method === 'getComments') {
         const title = document.querySelector('.content-title') ? document.querySelector('.content-title').innerText : ''
-        const items = [...document.querySelector('.comments__content').querySelectorAll('.comment')]
+        const items = [...document.querySelector('.comments__content_wrapper').querySelectorAll('.comment')]
         const data = items.reduce((result, item) => {
-            if (item.querySelector('.comment__avatar .andropov_image'))
+            if (item.querySelector('.comment__author'))
                 result.push({
                     id: item.getAttribute('data-id'),
                     date: item.getAttribute('data-date'),
                     userid: item.getAttribute('data-user_id'),
                     rating: item.getAttribute('data-rating'),
                     replyto: item.getAttribute('data-reply_to'),
-                    text: item.querySelector('.comment__text').innerHTML,
-                    img: item.querySelector('.comment__avatar > .andropov_image').getAttribute('data-image-src'),
-                    username: item.querySelector('.comment-user__name').innerText,
-                    userlink: item.querySelector('.comment-user').getAttribute('href'),
+                    text: item.querySelector('.comment__text') ? item.querySelector('.comment__text').innerHTML : '',
+                    img: item.querySelector('.comment__avatar').getAttribute('style').substring(23, 87),
+                    username: item.querySelector('.comment__author').innerText,
+                    userlink: item.querySelector('.comment__author').getAttribute('href'),
                     time: item.querySelector('time').innerText,
-                    votevalue: parseInt(item.querySelector('.vote__value__v').innerText.replace('–','-'), 10),
+                    votevalue: parseInt(item.querySelector('.vote__value').innerText.replace('–','-'), 10),
                     image: 
-                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_image') ? 
-                        [...item.querySelector('.comment__space').querySelectorAll('.comment__media .andropov_image')].map(el => el.getAttribute('data-image-src')) : null,
+                        item.querySelector('.comment__media .andropov_image') ? 
+                        [...item.querySelectorAll('.comment__media .andropov_image')].map(el => el.getAttribute('data-image-src')) : null,
                     video: 
-                        item.querySelector('.comment__space').querySelector('.comment__media .andropov_video') ? 
-                        [...item.querySelector('.comment__space').querySelectorAll('.comment__media .andropov_video')].map(el => el.getAttribute('data-video-mp4')) : null
+                        item.querySelector('.comment__media .andropov_video') ? 
+                        [...item.querySelectorAll('.comment__media .andropov_video')].map(el => el.getAttribute('data-video-mp4')) : null
                 })
             
             return result
